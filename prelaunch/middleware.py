375be8498334bca27e1e500f95acb8338019d340
Johan Beyers <jbeyers@juizi.com>
2011-04-22 20:03:21 +0200

    Middleware to put the referrer code in a cookie

---
+++ prelaunch/middleware.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from settings import *
+
+class PrelaunchMiddleware(object):
+    """ Adds a cookie to the response if a referrer parameter is present.
+    """
+
+    def process_response(self, request, response):
+        """ Add or change the cookie.
+        """
+        prelaunch_parameter = getattr(settings,
+                                      'PRELAUNCH_PARAMETER_NAME',
+                                      PRELAUNCH_PARAMETER_NAME)
+        print prelaunch_parameter
+
+        referrer = request.REQUEST.get(prelaunch_parameter, None)
+        if referrer and not request.COOKIES.get('prelaunch_referrer'):
+            response.set_cookie('prelaunch_referrer', referrer)
+        return response
