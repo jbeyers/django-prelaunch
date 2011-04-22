373c43290f4e028a54282808de41d21acb4cf720
Johan Beyers <jbeyers@juizi.com>
2011-04-22 20:02:09 +0200

    Setting for referral parameter added, and better explanations

--- prelaunch/settings.py
+++ prelaunch/settings.py
@@ -1,6 +1,17 @@
-# The numbers and letters to use as digits. a string, with no repeats.
-PRELAUNCH_DIGITS = '23456789abcdefghjklmnpqrstuvwxyz'
+# The following settings are the defaults. If you want to override them, you
+# can copy them to your main settings.py and adjust as needed.
 
-# Offset the hash-code with this amount. This is to prevent single-digit
-# or double-digit hashes, if you care about that kind of thing.
+# The numbers and letters allowed in the referral code. A string, with no
+# repeats. The string below consists of numbers and lower-case letters, with 0,
+# o, 1, i and l removed to make it easier to copy by hand without confusion.
+PRELAUNCH_DIGITS = '23456789abcdefghjkmnpqrstuvwxyz'
+
+# Offset the referral code with this amount. This is to prevent single-digit
+# or double-digit codes, if you care about that kind of thing.
 PRELAUNCH_OFFSET = 1200
+
+# The referral code typically looks something like this by default:
+# http://yourdomain.com/?referrer=3hg6sl
+# You can change the parameter name to something that does not clash with other
+# apps or looks nicer.
+PRELAUNCH_PARAMETER_NAME = 'referrer'
