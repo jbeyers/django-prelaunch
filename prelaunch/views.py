daaac943aaad7040bd1c6db8fa3e5cc658ecee6f
Johan Beyers <jbeyers@juizi.com>
2011-04-23 09:25:12 +0200

    Removed print statement

--- prelaunch/views.py
+++ prelaunch/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render_to_response
 
 def referrer(request, referrer):
-    print referrer
     response = HttpResponseRedirect('/')
     if not request.get_cookie('prelaunch_referrer'):
         response.set_cookie('prelaunch_referrer', referrer)
